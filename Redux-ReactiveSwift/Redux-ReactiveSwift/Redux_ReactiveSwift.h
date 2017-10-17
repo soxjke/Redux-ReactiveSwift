a3d4e4cb3d454dedb0dc2c301cc6feee425f3b1a
Petro Korienev <soxjke@gmail.com>
2017-10-17 15:30:02 +0100

    Restructured for easier Travis build & faster Tests run

---
+++ Redux-ReactiveSwift/Redux-ReactiveSwift/Redux_ReactiveSwift.h
@@ -0,0 +1,19 @@
+//
+//  Redux_ReactiveSwift.h
+//  Redux-ReactiveSwift
+//
+//  Created by Petro Korienev on 10/17/17.
+//  Copyright © 2017 Petro Korienev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Redux_ReactiveSwift.
+FOUNDATION_EXPORT double Redux_ReactiveSwiftVersionNumber;
+
+//! Project version string for Redux_ReactiveSwift.
+FOUNDATION_EXPORT const unsigned char Redux_ReactiveSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Redux_ReactiveSwift/PublicHeader.h>
+
+
