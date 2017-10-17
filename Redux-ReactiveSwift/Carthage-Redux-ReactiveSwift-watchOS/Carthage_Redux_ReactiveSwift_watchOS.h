e67c0d89c5839210d91189bc5e80f915a77accab
Petro Korienev <soxjke@gmail.com>
2017-10-17 22:19:02 +0100

    Reworked simultaneous CocoaPods & Carthage support to be spread among multiple targets and schemes

---
+++ Redux-ReactiveSwift/Carthage-Redux-ReactiveSwift-watchOS/Carthage_Redux_ReactiveSwift_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  Carthage_Redux_ReactiveSwift_watchOS.h
+//  Carthage-Redux-ReactiveSwift-watchOS
+//
+//  Created by Petro Korienev on 10/17/17.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Carthage_Redux_ReactiveSwift_watchOS.
+FOUNDATION_EXPORT double Carthage_Redux_ReactiveSwift_watchOSVersionNumber;
+
+//! Project version string for Carthage_Redux_ReactiveSwift_watchOS.
+FOUNDATION_EXPORT const unsigned char Carthage_Redux_ReactiveSwift_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage_Redux_ReactiveSwift_watchOS/PublicHeader.h>
+
+
---
+++ Redux-ReactiveSwift/CocoaPods-Redux-ReactiveSwift-macOS/CocoaPods_Redux_ReactiveSwift_macOS.h
@@ -0,0 +1,18 @@
+//
+//  CocoaPods_Redux_ReactiveSwift_macOS.h
+//  CocoaPods-Redux-ReactiveSwift-macOS
+//
+//  Created by Petro Korienev on 10/17/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaPods_Redux_ReactiveSwift_macOS.
+FOUNDATION_EXPORT double CocoaPods_Redux_ReactiveSwift_macOSVersionNumber;
+
+//! Project version string for CocoaPods_Redux_ReactiveSwift_macOS.
+FOUNDATION_EXPORT const unsigned char CocoaPods_Redux_ReactiveSwift_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPods_Redux_ReactiveSwift_macOS/PublicHeader.h>
+
+
